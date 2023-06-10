c638de3c8bdd39dcc3d52be1c2fc31bf9d59c783
Andrew Neophytou <andrew.neo82@gmail.com>
2023-06-10 00:27:04 +0000

    grabbed button elements

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// page Elements
+//get the buttons
+const rockSelector = document.getElementById("rock")
+const paperSelector = document.getElementById("paper")
+const scissorsSelector = document.getElementById("scissors")
+
