db587fc807b387b8279e6b0ec40aad5c9da2af9d
Andrew Neophytou <andrew.neo82@gmail.com>
2023-06-14 08:41:53 +0000

    fixed variable names in js file

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const playerPick = document.getElementById("player_choice");
+const CompuetPick = document.getElementById("computer_choice");
+const yourScore = document.getElementById("player_score");
+const cpuscore = document.getElementById("cpu_score");
+const restartGame = document.getElementById("restart_button");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+let playerScore = 0;
+let ComputerScore = 0;
