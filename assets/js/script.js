df78e7232962b28eefea3cc13b502f8e1edfc257
Andrew Neophytou <andrew.neo82@gmail.com>
2023-06-14 09:19:59 +0000

    add function for computer turn choice and console log to ensure generating random selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+const choices = ["rock", "paper", "scissors"]
+
 const playerPick = document.getElementById("player_choice");
-const CompuetPick = document.getElementById("computer_choice");
+const ComputerPick = document.getElementById("computer_choice");
 const yourScore = document.getElementById("player_score");
 const cpuscore = document.getElementById("cpu_score");
 const restartGame = document.getElementById("restart_button");
@@ -9,3 +11,12 @@ const scissors = document.getElementById("scissors");
 
 let playerScore = 0;
 let ComputerScore = 0;
+
+function computerTurn () {
+    const choices = ["rock", "paper", "scissors"];
+    let cTurn = Math.floor(Math.random()* choices.length);
+    return choices[cTurn];
+   
+}
+let test = computerTurn()
+console.log(test)
