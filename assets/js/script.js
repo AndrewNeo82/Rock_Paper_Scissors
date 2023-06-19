cebcbe0d2f1c2430d6b3f7a522b3325c178b8458
Andrew Neophytou <andrew.neo82@gmail.com>
2023-06-19 12:09:05 +0000

    add add choices map woth icons to display choices and add event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 const choices = ["rock", "paper", "scissors"]
+const  choicesMap = {
+    rock: '<i class="fa-solid fa-hand-back-fist" style="color: #00203fff;"></i>',
+    paper: '<i class="fa-solid fa-hand" style="color: #00203fff;"></i>',
+    scissors: '<i class="fa-solid fa-hand-scissors" style="color: #00203fff;"></i>'
+  };
+  
 
 const playerPick = document.getElementById("player_choice");
 const ComputerPick = document.getElementById("computer_choice");
@@ -9,14 +15,28 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
+
+
 let playerScore = 0;
 let ComputerScore = 0;
 
 function computerTurn () {
     const choices = ["rock", "paper", "scissors"];
     let cTurn = Math.floor(Math.random()* choices.length);
+    document.getElementById("player_choice").innerHTML= choicesMap[computerTurn]
     return choices[cTurn];
    
 }
-let test = computerTurn()
-console.log(test)
+
+
+
+document.getElementById('rock').addEventListener('click', function () {
+    gamePlay('rock');
+  });
+  document.getElementById('paper').addEventListener('click', function () {
+    gamePlay('paper');
+  });
+  document.getElementById('scissors').addEventListener('click', function () {
+    gamePlay('scissors');
+  });
+  
