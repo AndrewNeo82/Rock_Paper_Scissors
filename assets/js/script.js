1835ae2a52125c75da9f4a34ef29476e1fffae23
Andrew Neophytou <andrew.neo82@gmail.com>
2023-06-22 12:10:50 +0000

    fix spelling mistakes in code comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// choices
+// Choices
 
 const choices = ["rock", "paper", "scissors"];
 const choicesMap = {
@@ -7,7 +7,7 @@ const choicesMap = {
   scissors: '<i class="fa-solid fa-hand-scissors" style="color: #00203fff;"></i>'
 };
 
-// dom elementds
+// Get Dom Elements
 
 const playerPick = document.getElementById("player_choice");
 const computerPick = document.getElementById("cpu_choice");
@@ -20,11 +20,10 @@ const scissorsBtn = document.getElementById("scissors");
 const message = document.getElementById("message")
 const choose = document.getElementById("choose")
 
-
 let playerScore = 0;
 let computerScore = 0;
 
-// function to handle computer choice
+// Function to handle computer choice
 function computerTurn() {
   const choices = ["rock", "paper", "scissors"];
   let cTurn = Math.floor(Math.random() * choices.length);
@@ -32,7 +31,7 @@ function computerTurn() {
   return choices[cTurn];
 }
 
-// function to handle choice of win messages 
+// Function to handle choice of win messages 
 function pickMessage() {
   const winMessages = [
     "Congratulations! You won!",
@@ -49,7 +48,7 @@ function pickMessage() {
   return winMessage;
 }
 
-// function to pick a message to display if the computer wins the round 
+// Function to pick a message to display if the computer wins the round 
 
 function pickLoseMessage() {
   const loseMessages = [
@@ -67,7 +66,7 @@ function pickLoseMessage() {
 }
 
 
-/* function to handle player choice and the main game logic which 
+/* Function to handle player choice and the main game logic which 
  calls the functions that handle if the player wins loses or draws the round */
 
 function gamePlay(playerChoice) {
@@ -89,7 +88,7 @@ function gamePlay(playerChoice) {
   endGame()
 }
 
-/* function called when the player wins the round  displays a message increments player 
+/* Function called when the player wins the round,  displays a message increments player 
 score and changes the border of the last pick display */
 function winner () {
  const winMessage = pickMessage();
@@ -99,8 +98,8 @@ function winner () {
     playerPick.style.borderColor = "green"; 
     computerPick.style.borderColor = "red";
 }
-/* function called when the player loses the round  displays a message increments computer 
-score and changes the border of the last pick display */
+/* Function called when the player loses the round,  displays a message increments computer 
+score and changes the border of the last pick display. */
 
 function loser (){
 const loseMessage = pickLoseMessage();
@@ -111,17 +110,15 @@ const loseMessage = pickLoseMessage();
     computerPick.style.borderColor = "green";
 }
 
-// function called when the round is  a draw, displays a message and changed border of last pick.
+// Function called when the round is  a draw, displays a message and changed border of last pick.
 
 function draw() {
     message.textContent = "A Draw! Everybody Loses!";
     playerPick.style.borderColor = "black"; 
     computerPick.style.borderColor = "black";
-    
-
 }
 
-/* check if score of 5 has been reached and if it has ends the game 
+/* Check if score of 5 has been reached and if it has ends the game 
 hides the choice buttons displays the message game over and shows the reset button */
 
 
@@ -141,12 +138,10 @@ function endGame() {
     scissorsBtn.style.display = "none";
     restartGame.style.display = "block";
     choose.innerHTML = "<h2>Game Over!</h2>";
-
-  }
-
+}
 }
 
-// function to reset the game, when the reset button is clicked the game is restored to its default values
+// Function to reset the game, when the reset button is clicked the game is restored to its default values
 
 function resetGame() {
   playerScore = 0;
@@ -165,7 +160,7 @@ function resetGame() {
   choose.innerHTML = "<h2>Choose Rock Paper Scissors</h2>";
   
 }
-// event listeners for choice buttons
+// Event listeners for choice buttons
 
 document.getElementById('rock').addEventListener('click', function () {
   gamePlay('rock');
@@ -177,7 +172,7 @@ document.getElementById('scissors').addEventListener('click', function () {
   gamePlay('scissors');
 });
 
-// eventy listener for button to play again 
+// Event listener for button to play again 
 document.getElementById("restart_button").addEventListener('click', function () {
   resetGame();
 });
