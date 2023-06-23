f8243a372af6990659e366e604bffda9c07b0f66
Andrew Neophytou <andrew.neo82@gmail.com>
2023-06-23 06:48:28 +0000

    style landscape for mobile devices to ensure all content fits onto the screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -147,7 +147,7 @@ function resetGame() {
   playerScore = 0;
   computerScore = 0;
   playerPick.style.borderColor = "#adefd1ff";
-  playerPick.innerHTML = "<p>You Pick</p>";
+  playerPick.innerHTML = "<p>Your Pick</p>";
   computerPick.style.borderColor = "#adefd1ff";
   computerPick.innerHTML = "<p>Computer Pick</p>";
   yourScore.textContent = "Player Score: " + playerScore;
